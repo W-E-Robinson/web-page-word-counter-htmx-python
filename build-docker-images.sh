188100a65c1dc57b1cf828cf9c36c52ba6715531
Will Robinson <william.ellis.robinson@gmail.com>
2025-01-23 21:03:44 +0000

    chore: updated building of images to pull latest image of mongodb

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build --pull --no-cache -t web-page-word-counter-htmx -f frontend/Dockerfile frontend/
 docker build --pull --no-cache -t web-page-word-counter-python -f backend/Dockerfile backend/
+docker pull mongo:latest
