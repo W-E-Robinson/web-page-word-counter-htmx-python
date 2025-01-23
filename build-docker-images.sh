929aef4ed762c325255ba0657d2265ed5166ce0d
Will Robinson <william.ellis.robinson@gmail.com>
2025-01-23 20:20:02 +0000

    feat: moved index.* into frontend directory and created Dockerfile
    moved backend docker compose into top level and added htmx service

---
+++ build-docker-images.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build --pull --no-cache -t web-page-word-counter-htmx -f frontend/Dockerfile frontend/
+docker build --pull --no-cache -t web-page-word-counter-python -f backend/Dockerfile backend/
---
+++ frontend/README.md
@@ -0,0 +1,10 @@
+this is a readme for the fronten
+
+# web-page-word-counter-frontend-htmx
+
+## Development
+
+### Serve files through local server (below or equivalent)
+```sh
+python3 -m http.server 8000
+```
